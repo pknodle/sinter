d1321edb13295268046aa61d6b0fcd70e78aaf02
Philip Knodle <pknodle@gmail.com>
2016-08-15 00:28:18 -0400

    Added Heat of fusion to calculation.
    This is a final sainity check and it says that I might get away with ~1W
    of optical power to do sintering.
    
    The next step is to check the basic dynamics of the system to see if they
    will work.

--- sainity_check.py
+++ sainity_check.py
@@ -10,6 +10,7 @@ from sympy.physics.units import *
 
 density_of_al = 2.70*gram/cm**3
 specific_heat_al = 0.9 * joule/(gram * K)
+heat_of_fusion_al = 386.9 * joule / gram
 diameter_of_spot = 0.4 *mm
 depth_of_spot = 0.4*mm
 
@@ -20,10 +21,15 @@ mass_of_spot = density_of_al * volume_of_spot
 # I want to raise the mass of the spot 100K in 100ms
 # What power do I need?
 
-energy_needed = specific_heat_al * mass_of_spot * 800*K
+energy_needed_to_heat = specific_heat_al * mass_of_spot * 660*K
+
+# You need some extra energy in order to break the bonds of soilid and turn in to a liquid
+# ie, the Enthalpy of fusion.
+energy_needed_to_melt = heat_of_fusion_al * mass_of_spot
+energy_needed = energy_needed_to_heat + energy_needed_to_melt
 power_needed = energy_needed / (100*milli*seconds)
 
-print('1 Watt: {} '.format(watt))
+
 print('You will need {} '.format(power_needed))
 
 
